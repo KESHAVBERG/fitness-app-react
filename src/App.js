ecbb5809c5114402b05805c0feffdfa697148f45
KESHAVBERG <kingkeshav7@gmail.com>
2022-07-17 11:26:37 +0530

    nav on progrees

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import {Route, Routes} from 'react-router-dom';
 import {Box} from '@mui/material';
-import Navbar from './componets/Navbar'
+import Navbar from './componets/Navbar';
+import Home from './pages/Home';
+import ExcerciseDetails from './pages/ExcerciseDetails';
+import Footer from './componets/Footer';
 function App() {
   return (
-   <Box>
+   <Box width="400px" sx = {{width:{xl:'1400px'}}} m = 'auto'>
     <Navbar />
+    <Routes>
+      <Route path='/'element ={<Home/>}/>
+      <Route path='/excercise/:id' element = {<ExcerciseDetails />} />
+    </Routes>
+    <Footer />
    </Box>
   );
 }
--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -1,9 +1,43 @@
 
+import { Stack } from '@mui/material';
+import { padding } from '@mui/system';
 import React from 'react'
+import { Link } from 'react-router-dom';
+
+import Logo from '../assets/images/Logo.png';
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <Stack
+    direction='row'
+    justifyContent='space-around'
+    sx={
+      {
+        gap:{sm:'122px', xs:'40px'},
+        mt:{sm:'32px',xs:'20px'},
+        justifyContent:'none'
+      }
+    }
+    px='20px'
+
+    >
+      <Link to='/'>
+        <img src={Logo} style={{ margin: '10px 20px' }} />
+      </Link>
+      <Stack
+        direction='row'
+        gap='40px'
+        alignItems='flex-end'
+        fontSize='20px'
+      >
+        <Link to='/' style={{
+          textDecoration: 'none', color: '#FAC970'
+        }}>Home</Link>
+        <a href="#excercies" style={{
+          textDecoration: 'none', color: '#FAC970'
+        }}>Excercises</a>
+      </Stack>
+    </Stack>
   )
 }
 
