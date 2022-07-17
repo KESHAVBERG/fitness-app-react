8ad8ca9d7d2730aaa2b4ec54ea97d090dd9c6a1f
KESHAVBERG <kingkeshav7@gmail.com>
2022-07-17 16:59:47 +0530

    button color change

--- src/componets/Banner.jsx
+++ src/componets/Banner.jsx
@@ -1,20 +1,41 @@
-import { Stack, Box, Typography } from '@mui/material'
+import { Stack, Box, Typography, Button, createTheme, ThemeProvider } from '@mui/material'
 import React from 'react'
 
+const theme = createTheme({
+    status: {
+        danger: '#FFB124',
+    },
+    palette: {
+        primary: {
+            main: '#0971f1',
+            darker: '#053e85',
+        },
+        neutral: {
+            main: '#FFB124',
+            contrastText: '#fff',
+        },
+    },
+});
 
 const Banner = () => {
-  return (
-    <Box sx={{
-        mt:{lg:'212px', xs:'70px'},
-        ml:{lg:'70px', xs:'50px'}
-    }} position='relative'
-    p = '20px'
->
-        <Typography>
-            Let's Sweat
-        </Typography>
-    </Box>
-  )
+    return (
+        <Box sx={{
+            mt: { lg: '212px', xs: '70px' },
+            ml: { lg: '70px', xs: '50px' }
+        }} position='relative'
+            p='20px'
+        >
+            <Typography fontSize='30px' fontWeight='600' color='orange' mb={3}>
+                Let's Sweat
+            </Typography>
+
+            <ThemeProvider theme={theme}>
+                <Button color="neutral" variant="contained" href='#excercises'>
+                    Search Excercises
+                </Button>
+            </ThemeProvider>
+        </Box>
+    )
 }
 
 
