1c665d266aa2095dfc1dbe1f0cdfcc88744c839b
KESHAVBERG <kingkeshav7@gmail.com>
2022-08-08 11:29:34 +0530

    gifs done

--- src/componets/ExcerciseCard.jsx
+++ src/componets/ExcerciseCard.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Box, Typography, Stack } from '@mui/material'
+import { Button, Typography, Stack } from '@mui/material'
+
+
 const ExcerciseCard = ({excercise}) => {
   return (
-    <div>ExcerciseCard</div>
+    <Link style={{textDecoration: 'none'}} to={`excercise/${excercise.id}`}>
+      <img src={excercise.gifUrl} alt={excercise.name} loading="Lazy" />
+      <Stack direction="row">
+        <Button sx = {{
+          ml:"22px", color:"white", backgroundColor:"#FBC461", fontSize:"14px", borderRadius:"30px", textDecoration:"none"
+        }}>
+          {excercise.bodyPart}
+        </Button>
+
+        <Button sx = {{
+          ml:"22px", color:"white", backgroundColor:"#FCA370", fontSize:"14px", borderRadius:"30px", textDecoration:"none"
+        }}>
+          {excercise.target}
+        </Button>
+      </Stack>
+      <Typography sx={{
+        color:"black",
+        textTransform:"capitalize",
+        ml:"25px",
+        mt:"10px",
+        fontWeight:"bold"
+      }}>
+        {excercise.name}
+      </Typography>
+    </Link>
   )
 }
 
