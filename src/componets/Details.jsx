bee5df949bb01d01e1c76f1115a55275d08f9b46
KESHAVBERG <kingkeshav7@gmail.com>
2022-08-20 20:57:30 +0530

    working-on-detials

--- src/componets/Details.jsx
+++ src/componets/Details.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Typography, Box, Button, Stack } from '@mui/material';
 
-const Details = () => {
+import BodyPart from '../assets/icons/body-part.png'
+import target from '../assets/icons/target.png'
+import equipment from '../assets/icons/equipment.png'
+
+const Details = ({ exerciseDetail }) => {
+  const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+
   return (
-    <div>Details</div>
+    <Stack gap="60px" sx={{flexDirection:{lg:'row'}, p:"20px", alignItems:"center"}}>
+      <img src={gifUrl} alt={name} loading="lazy" className='detail-image' />
+
+    </Stack>
   )
 }
 
--- src/pages/ExcerciseDetails.jsx
+++ src/pages/ExcerciseDetails.jsx
@@ -6,13 +6,19 @@ import Details from '../componets/Details';
 import ExcerciseVideos from '../componets/ExcerciseVideos';
 
 const ExcerciseDetails = () => {
-  const [exerciseDetail, SetExerciseDetail] = useEffect([]);
+  const [exerciseDetail, SetExerciseDetail] = useState({});
   const {id} = useParams();
 
   useEffect(()=>{
     const fetchExcerciseData = async()=>{
+      const excerciseurl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+      const excerciseDetailData = await fetchData(`${excerciseurl}/exercises/exercise/${id}`, options);
 
+      console.log(excerciseDetailData);
+      SetExerciseDetail(excerciseDetailData)
     }
+    fetchExcerciseData();
   }, [])
 
   return (
